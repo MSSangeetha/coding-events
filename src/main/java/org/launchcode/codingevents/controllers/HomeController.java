ffd1e037b4d8fd0a95e5c131fe6d24e867481d8e
MSSangeetha <sangeethasanthosh.m@gmail.com>
2021-09-05 23:01:19 -0500

    pusing coding events first time

---
+++ src/main/java/org/launchcode/codingevents/controllers/HomeController.java
@@ -0,0 +1,12 @@
+package org.launchcode.codingevents.controllers;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class HomeController {
+
+    //First Method
+    public String index(){
+        return "index";
+    }
+}
